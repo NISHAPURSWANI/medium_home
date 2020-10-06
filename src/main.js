62c0210a9f9fcbc4d9e09fe572ad89c57a3affa4
NISHAPURSWANI <2016.nisha.purswani@ves.ac.in>
2020-10-06 22:58:20 +0530

    layout and positioning of the home page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
--- src/views/Medium_Home.vue
+++ src/views/Medium_Home.vue
@@ -1,132 +1,288 @@
 <template>
-    <div>
-        <b-container class="main_container">
-            <b-row class="newrow">
-                  <!-- <img
+  <div>
+    <b-container class="main_container">
+      <b-row class="newrow">
+        <!-- <img
                   alt="Vue logo"
                   src="../assets/medium.png" class="mediumimg"
                   style="height:65px;width:150px;border:none"
                  
                 />   -->
-               <span class="mtext">Medium</span> 
-               <!-- <div class="iconsrhs">
+        <span class="mtext">Medium</span>
+        <!-- <div class="iconsrhs">
                    <i class="fas fa-search"></i>
                </div> -->
+      </b-row>
+      <b-row class="firstarea">
+        <b-col class="cols"
+          ><img
+            alt="Vue logo"
+            src="../assets/refimg1.png"
+            style="height:222px;width:374px;"
+          /><br />The first thing that you will encounter is the difference in
+          initializing your app. In Vue 2 you have to use Vue constructor with a
+          render function and the $mount method like this
+        </b-col>
 
-                
-                
-
-            </b-row>
-            <b-row class="firstarea">
-            <b-col class="cols"><img
-                  alt="Vue logo"
-                  src="../assets/refimg1.png" 
-                  style="height:222px;width:374px;"
-
-                 
-                /><br>The first thing that you will encounter is the difference in initializing your app. In Vue 2 you have to use Vue constructor with a render function and the $mount method like this
-</b-col>
-
-            <b-col class="cols"><div class="imgincol"><img
-                  alt="Vue logo"
-                  src="../assets/refimg1.png" 
-                  style="height:100px;width:100px;"
-
-                 
-                /></div>
-               
-                </b-col>
-            <b-col class="cols">1st column</b-col>
-            </b-row>
-            <b-row class="trendarea">
-             <b-col class="cols"><img
-                  alt="Vue logo"
-                  src="../assets/refimg1.png" 
-                  style="height:222px;width:374px;"
-
-                 
-                /><br>The first thing that you will encounter is the difference in initializing your app. In Vue 2 you have to use Vue constructor with a render function and the $mount method like this
-</b-col>
-            <b-col class="cols">1st column</b-col>
-            <b-col class="cols">1st column</b-col>
-            </b-row>
-            <b-row class="lastarea">
-             <b-col class="cols"><img
-                  alt="Vue logo"
-                  src="../assets/refimg1.png" 
-                  style="height:222px;width:374px;"
-
-                 
-                /><br>The first thing that you will encounter is the difference in initializing your app. In Vue 2 you have to use Vue constructor with a render function and the $mount method like this
-</b-col>
-            <b-col class="cols">1st column</b-col>
-            </b-row>
-        </b-container>
+        <b-col class="cols">
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+            <div class="col-8">
+              <span class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle, </span
+              >
+            </div>
+            <div class="col-4">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+            <div class="col-8">
+              <span class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle, </span
+              >
+            </div>
+            <div class="col-4">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+            <div class="col-8">
+              <span class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle, </span
+              >
+            </div>
+            <div class="col-4">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+            <div class="col-8">
+              <span class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle,</span
+              >
+            </div>
+            <div class="col-3">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+          </b-row>
+          <b-row class="content">
+            <!-- <img alt="Vue logo" src="../assets/refimg1.png" class="imgincol" /> -->
+          </b-row>
+        </b-col>
+        <b-col class="cols">1st column</b-col>
+      </b-row>
+      <b-row class="trendarea">
+        <b-col class="cols">
+          <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >01 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row>
+        </b-col>
+        <b-col class="cols">    <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >02 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row></b-col>
+        <b-col class="cols">    <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >03 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row></b-col>
         
-    </div>
+      </b-row>
+       <b-row class="trendareanext">
+        <b-col class="cols">    <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >04 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row></b-col>
+        <b-col class="cols">    <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >05 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row></b-col>
+        <b-col class="cols">    <b-row>
+          <div class="col-1">
+              <span class="acontent"
+                >06 </span
+              >
+            </div>
+            <div class="col-10">
+              The Glory of heartbreak of a dog
+            </div>
+          </b-row></b-col>
+        
+      </b-row>
+      <b-row class="lastarea"> 
+    <div class="col-9">
+      <b-row>
+              <div class="col-7">
+              <p class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle, </p
+              >
+            </div>
+            <div class="col-5">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+      </b-row>
+       <b-row>
+              <div class="col-7">
+              <p class="acontent"
+                >Above way is pretty easy, but it has a downside: it will bundle
+                all available icons to your final bundle, </p
+              >
+            </div>
+            <div class="col-5">
+              <img
+                alt="Vue logo"
+                src="../assets/refimg1.png"
+                class="imgincol"
+              />
+            </div>
+      </b-row>
+      
+            </div>
+            <div class="col-3">
+              The Glory of heartbreak of a dog
+            </div>
+  </b-row>
+
+    </b-container>
+  </div>
 </template>
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed");
 
-.main_container{
-   
-    padding-top:0px;
-    margin:0 64px;
-    margin-top:24px; 
-    margin-bottom:24px;
-
-
+.main_container {
+  padding-top: 0px;
+  margin: 0 64px;
+  margin-top: 24px;
+  margin-bottom: 24px;
 }
 /* .mediumimg{
     border:0px;
 } */
-.newrow{
-    margin-top:24px; 
-    margin-bottom:24px;
-    /* padding-top:10px;       padding-left:10px; */
-    display:flex;
-    height:65px;
-    border:1px solid;
-    font-weight: bold;
+.newrow {
+  margin-top: 24px;
+  margin-bottom: 24px;
+  /* padding-top:10px;       padding-left:10px; */
+  display: flex;
+  height: 65px;
+  /* border: 1px solid; */
+  font-weight: bold;
   /* font-family: 'Roboto', sans-serif; */
   /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
-  
-
-    
 }
-.mtext{
-padding-top:10px;
-padding-right:16px;
-/* border-right:1px solid; */
-font-size:27px;
-height:27px;
+.mtext {
+  padding-top: 10px;
+  padding-right: 16px;
+  border-right:1px solid;
+  font-size: 27px;
+  height: 27px;
 }
-.iconsrhs{
-     display: flex;
+.iconsrhs {
+  display: flex;
   float: right;
   margin-right: 15px;
+}
+.cols {
+  border: 1px solid;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.trend {
+  padding-top: 40px;
+}
+.firstarea {
+  padding-bottom: 40px;
+}
+.lastarea {
+  padding-top: 56px;
+}
+.imgincol {
+  display: flex;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  height: 100px;
+  width: 100px;
+  justify-content: flex-end;
+}
+.content {
+  /* border: 1px solid; */
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.acontent{
+    padding-right:2px;
+    padding-top:10px;
 
 }
-.cols{
-    border:1px solid;
-    padding-left:16px;
-    padding-right:16px;
-    padding-top:10px;
-    padding-bottom:10px;
-}
-.trend{
-    padding-top:40px;
-}
-.firstarea{
-    padding-bottom: 40px;
-}
-.lastarea{
-    padding-top:56px;
-}
-.imgincol{
-    display:flex;
-    float:right;
-    padding-top:10px;
-    padding-bottom:20px;
+.trendareanext{
+ padding-top:20px;
 }
 </style>
